deb6fb82d64d0a4a7b73cc159406187a5abd1d4a
wudunkai <wdk15070234551@163.com>
2022-01-11 17:29:47 +0800

    添加 promise方法

--- JavaScript/Promise.js
+++ JavaScript/Promise.js
@@ -91,13 +91,101 @@ class MyPromise {
     // console.log(thenPromise);
     // return thenPromise
   }
+  static all(promises) {
+    const result = []
+    let count = 0
+    // 接收一个promise数组，数组中如有非promise项，则此项目做成功
+    return new MyPromise((resolve, reject) => {
+      const addData = (index, value) => {
+        result[index] = value
+        count++
+        if (count === promises.length) resolve(result)
+      }
+      // 如果所有Promise都成功，则返回成功结果数组
+      // 如果有一个Promise失败，则返回这个失败结果
+      promises.forEach((promise, index) => {
+        // console.log(promise);
+        if (promise instanceof MyPromise) {
+          promise.then(res => {
+            addData(index, res)
+          }, err => reject(err))
+        } else {
+          addData(index, promise)
+        }
+      })
+    })
+  }
+  static race(promises) {
+    // 接收一个promise数组，数组中如有非promise项，则此项当做成功
+    return new MyPromise((resolve, reject) => {
+      promises.forEach(promise => {
+        // 那个promise最快得到结果，就返回那个结果，无论成功失败
+        if (promise instanceof MyPromise) {
+          promise.then(res => {
+            resolve(res)
+          }, err => {
+            reject(err)
+          })
+        } else {
+          resolve(promise)
+        }
+      })
+    })
+  }
+  static allSettled(promises) {
+    // 接收一个promise数组，数组中如果非promise项，则此项当做成功
+    // 把每一个promise的结果，集合成数组，返回
+    return new Promise((resolve, reject) => {
+      const res = []
+      let count = 0
+      const addData = (status, value, i) => {
+        res[i] = {
+          status,
+          value
+        }
+        count++
+        if (count === promises.length) {
+          resolve(res)
+        }
+      }
+      promises.forEach((promise, i) => {
+        if (promise instanceof MyPromise) {
+          promise.then(res => {
+            addData('fulfilled', res, i)
+          }, err => {
+            addData('rejected', err, i)
+          })
+        } else {
+          addData('fulfilled', promise, i)
+        }
+      })
+    })
+  }
+  static any(promises) {
+    return new Promise((resolve, reject) => {
+      let count = 0
+      // 接收一个Promise数组，数组中如有非Promise项，则此项当做成功
+      promises.forEach((promise) => {
+        // 如果有一个Promise成功，则返回这个成功结果
+        promise.then(val => {
+          resolve(val)
+        }, err => {
+          count++
+          // 如果所有Promise都失败，则报错
+          if (count === promises.length) {
+            reject(new AggregateError('All promises were rejected'))
+          }
+        })
+      })
+    })
+  }
 }
 
 // new MyPromise((resolve, reject) => {
 //   setTimeout(() => {
 
 //     resolve(100) // 输出 状态：失败 值：300
-//   }, 100);
+//   }, 1000);
 //   // reject(100) // 输出 状态：成功 值：200
 //   // 这里可没搞反哦。真的搞懂了，就知道了为啥这里是反的
 // }).then(res => new MyPromise((resolve, reject) => setTimeout(() => {
@@ -106,8 +194,41 @@ class MyPromise {
 // ), err => new MyPromise((resolve, reject) => resolve(3 * res)))
 //   .then(res => console.log('成功', res), err => console.log('失败', err))
 
-new MyPromise((resolve, reject) => {
-  resolve(100) // 输出 状态：成功 值： 200
-  // reject(100) // 输出 状态：失败 值：300
-}).then(res => 2 * res, err => 3 * err)
-  .then(res => console.log('成功', res), err => console.log('失败', err))
+// new MyPromise((resolve, reject) => {
+//   reject(100) // 输出 状态：成功 值： 200
+//   // reject(100) // 输出 状态：失败 值：300
+// }).then(res => 2 * res, err => 3 * err)
+//   .then(res => console.log('成功', res), err => console.log('失败', err))
+
+// let promise1 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(100) // 输出 状态：失败 值：300
+//   }, 1000);
+//   // reject(100) // 输出 状态：成功 值：200
+//   // 这里可没搞反哦。真的搞懂了，就知道了为啥这里是反的
+// })
+
+// let promise2 = new MyPromise((resolve, reject) => {
+//   resolve(200) // 输出 状态：成功 值： 200
+//   // reject(100) // 输出 状态：失败 值：300
+// })
+// let promise3 = new MyPromise((resolve, reject) => {
+//   reject(300) // 输出 状态：成功 值： 200
+//   // reject(100) // 输出 状态：失败 值：300
+// })
+// // all
+// MyPromise.all([promise1, promise2, promise3]).then(posts => {
+//   console.log("all:", posts);
+// })
+// // race
+// MyPromise.race([promise1, promise2]).then(posts => {
+//   console.log("race:", posts);
+// })
+// // allSettled
+// MyPromise.allSettled([promise1, promise2]).then(posts => {
+//   console.log("allSettled:", posts);
+// })
+// // any
+// MyPromise.any([promise1, promise2, promise3]).then(posts => {
+//   console.log("any:", posts);
+// })
