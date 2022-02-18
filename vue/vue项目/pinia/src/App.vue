7d7cac226ec1d836d7d53ae48dd34d0d790fe49c
wudunkai <wdk15070234551@163.com>
2022-02-18 18:16:03 +0800

    学习列表管理

---
+++ vue/vue项目/pinia/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import PinIa from './components/PinIa.vue'
+import handleUpload from './components/handleUpload.vue'
+import { reactive, ref, toRefs } from 'vue';
+const form: any = {
+  error: ref<string>("aaa"),
+  errors: reactive({
+    name: '1',
+    password: '2'
+  })
+}
+const { error, errors } = form
+const { name } = toRefs(form.errors)
+console.log(name, form.errors);
+
+</script>
+
+<template>
+  {{ error }}
+  <input v-model="error" />
+  {{ name }}
+  <div v-for="(item, index) in errors" :key="index">
+    {{ item }}
+    <input v-model="errors[index]" />
+  </div>
+  <PinIa>
+    <template #user>user</template>
+    <template #login>login</template>
+  </PinIa>
+  <handleUpload />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue项目/pinia/src/components/PinIa.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useUserStore } from '../stores/PinIa'
+const user = useUserStore()
+user.login('321321')
+</script>
+<template>
+  qqq
+  <slot name="user"></slot>
+  <slot name="login"></slot>
+</template>
